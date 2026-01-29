c7732c5c3e071e63c85a72bfa3421ea567c77864
zorvi <aervxa@gmail.com>
2026-01-29 15:12:20 +0300

    Base layout

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <SidebarProvider style="--sidebar-width: 24rem">
+
+    <!-- Content -->
+    <main class="flex-1 grid place-content-center">
+      <p class="text-2xl">File upload</p>
+    </main>
+
+    <!-- Sidebar -->
+    <Sidebar side="right">
+      <SidebarHeader>header</SidebarHeader>
+      <SidebarContent class="bg-muted grid place-content-center">content</SidebarContent>
+      <SidebarFooter>footer</SidebarFooter>
+    </Sidebar>
+
+  </SidebarProvider>
 </template>
