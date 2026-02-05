2f13e06e260cca3a0066c86b6a56eb48a5ca4425
zorvi <aervxa@gmail.com>
2026-02-05 14:31:59 +0300

    add tooltips for "header" part of app

--- app/app.vue
+++ app/app.vue
@@ -116,108 +116,77 @@ const TITLE = "Image format converter";
 </script>
 
 <template>
-  <main class="flex size-full flex-1 overflow-hidden max-md:flex-col">
-    <!-- Title + Options Header (for small screens without sidebar) -->
-    <div class="flex flex-wrap items-center-safe justify-between gap-4 p-4">
-      <p class="text-xl font-semibold md:hidden">{{ TITLE }}</p>
+  <TooltipProvider>
+    <main class="flex size-full flex-1 overflow-hidden max-md:flex-col">
+      <!-- Title + Options Header (for small screens without sidebar) -->
+      <div class="flex flex-wrap items-center-safe justify-between gap-4 p-4">
+        <p class="text-xl font-semibold md:hidden">{{ TITLE }}</p>
 
-      <!-- Options (image select/change & output directory selection) -->
-      <div class="flex gap-2">
-        <!-- Select Image -->
-        <Button variant="secondary" @click="selectImage()">
-          <ImageUp v-if="imagePath" />
-          <ImagePlus v-else />
-        </Button>
+        <!-- Options (image select/change & output directory selection) -->
+        <div class="flex gap-2">
+          <!-- Select Image -->
+          <Tooltip>
+            <TooltipTrigger as-child>
+              <Button variant="secondary" @click="selectImage()">
+                <ImageUp v-if="imagePath" />
+                <ImagePlus v-else />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p class="font-medium">
+                <template v-if="imagePath">Change</template>
+                <template v-else>Select</template> Image
+              </p>
+            </TooltipContent>
+          </Tooltip>
 
-        <!-- Set Output Directory -->
-        <Button variant="secondary" size="icon" @click="setOutputDir">
-          <FolderInput />
-        </Button>
-      </div>
-    </div>
-
-    <!-- Image Selection/Viewer -->
-    <div class="flex flex-1 flex-col gap-2 p-4">
-      <!-- Image preview -->
-      <img
-        v-if="imagePath"
-        :src="convertFileSrc(imagePath)"
-        alt="uploaded image"
-        class="bg-muted/80 size-full rounded-xl object-contain"
-      />
-      <!-- Upload region -->
-      <div
-        @click="selectImage()"
-        v-else
-        ref="dropZone"
-        class="bg-muted/60 hover:border-primary/40 hover:bg-muted/80 relative flex size-full cursor-pointer flex-col items-center-safe justify-center-safe gap-4 rounded-xl border-2 border-dashed p-4"
-        :class="[dragActive && 'border-primary/40 bg-muted/80!']"
-      >
-        <Image :size="64" />
-        <p class="text-2xl font-semibold">Drag and drop any image</p>
-        <p
-          class="absolute bottom-4 max-w-prose text-center font-mono text-sm leading-relaxed opacity-40"
-        >
-          Supported formats: &nbsp; AVIF, BMP, GIF, JPG, JPEG, PNG, WebP
-        </p>
-      </div>
-    </div>
-
-    <!-- Options "Bottom bar" (for small screens without sidebar) -->
-    <div class="flex justify-between border-t p-4 md:hidden">
-      <!-- Select Format -->
-      <Select v-model="toFormat">
-        <SelectTrigger>
-          <SelectValue placeholder="Select Format" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Formats</SelectLabel>
-            <SelectItem value="jpg">JPG</SelectItem>
-            <SelectItem value="png">PNG</SelectItem>
-            <SelectItem value="webp">WEBP</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-
-      <!-- Fourmat (Format and Save) -->
-      <Button class="self-start" @click="save" :disabled="isSaving">
-        <Cpu />
-        Fourmat
-      </Button>
-    </div>
-
-    <!-- Options "Sidebar" -->
-    <div
-      class="flex w-2xs flex-col gap-6 overflow-auto border-l p-4 pb-8 max-md:hidden lg:w-xs xl:w-sm"
-    >
-      <p class="text-2xl font-bold">{{ TITLE }}</p>
-
-      <!-- Step 1: Select Image -->
-      <div class="flex flex-col gap-2">
-        <p
-          class="text-xl font-semibold"
-          :class="[imagePath && 'flex items-center-safe gap-2']"
-        >
-          Step 1
-          <Check v-if="imagePath" :size="18" class="text-primary" />
-        </p>
-        <Button class="self-start" variant="secondary" @click="selectImage()">
-          <template v-if="imagePath">Change</template>
-          <template v-else>Select</template> Image
-        </Button>
+          <!-- Set Output Directory -->
+          <Tooltip>
+            <TooltipTrigger as-child>
+              <Button variant="secondary" size="icon" @click="setOutputDir">
+                <FolderInput />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p class="font-medium">
+                <template v-if="outputDir">Change Output Folder</template>
+                <template v-else>Set Output Folder</template>
+              </p>
+            </TooltipContent>
+          </Tooltip>
+        </div>
       </div>
 
-      <!-- Step 2: Select Format -->
-      <div class="flex flex-col gap-2">
-        <p
-          class="text-xl font-semibold"
-          :class="[toFormat && 'flex items-center-safe gap-2']"
+      <!-- Image Selection/Viewer -->
+      <div class="flex flex-1 flex-col gap-2 p-4">
+        <!-- Image preview -->
+        <img
+          v-if="imagePath"
+          :src="convertFileSrc(imagePath)"
+          alt="uploaded image"
+          class="bg-muted/80 size-full rounded-xl object-contain"
+        />
+        <!-- Upload region -->
+        <div
+          @click="selectImage()"
+          v-else
+          ref="dropZone"
+          class="bg-muted/60 hover:border-primary/40 hover:bg-muted/80 relative flex size-full cursor-pointer flex-col items-center-safe justify-center-safe gap-4 rounded-xl border-2 border-dashed p-4"
+          :class="[dragActive && 'border-primary/40 bg-muted/80!']"
         >
-          Step 2
-          <Check v-if="toFormat" :size="18" class="text-primary" />
-        </p>
-        <p class="text-sm">Select format to convert to</p>
+          <Image :size="64" />
+          <p class="text-2xl font-semibold">Drag and drop any image</p>
+          <p
+            class="absolute bottom-4 max-w-prose text-center font-mono text-sm leading-relaxed opacity-40"
+          >
+            Supported formats: &nbsp; AVIF, BMP, GIF, JPG, JPEG, PNG, WebP
+          </p>
+        </div>
+      </div>
+
+      <!-- Options "Bottom bar" (for small screens without sidebar) -->
+      <div class="flex justify-between border-t p-4 md:hidden">
+        <!-- Select Format -->
         <Select v-model="toFormat">
           <SelectTrigger>
             <SelectValue placeholder="Select Format" />
@@ -231,46 +200,102 @@ const TITLE = "Image format converter";
             </SelectGroup>
           </SelectContent>
         </Select>
-      </div>
 
-      <!-- Step 3: Set Output Directory -->
-      <div class="flex flex-col gap-2">
-        <p
-          class="text-xl font-semibold"
-          :class="[outputDir && 'flex items-center-safe gap-2']"
-        >
-          Step 3
-          <Check v-if="outputDir" :size="18" class="text-primary" />
-        </p>
-        <p v-if="!outputDir" class="text-sm">Defaults to image's path</p>
-        <div class="flex gap-2">
-          <Button variant="secondary" @click="setOutputDir">
-            <template v-if="outputDir">Change Folder</template>
-            <template v-else>Set Output Folder</template>
-          </Button>
-          <Button
-            v-if="outputDir"
-            variant="secondary"
-            size="icon"
-            @click="outputDir = ''"
-          >
-            <X />
-          </Button>
-        </div>
-        <p v-if="outputDir" class="font-mono text-sm wrap-anywhere opacity-40">
-          {{ outputDir }}
-        </p>
-      </div>
-
-      <!-- Step 4: Fourmat (Format and Save) -->
-      <div class="flex flex-col gap-2">
-        <p class="text-xl font-semibold">Step 4</p>
+        <!-- Fourmat (Format and Save) -->
         <Button class="self-start" @click="save" :disabled="isSaving">
           <Cpu />
           Fourmat
         </Button>
       </div>
-    </div>
-  </main>
+
+      <!-- Options "Sidebar" -->
+      <div
+        class="flex w-2xs flex-col gap-6 overflow-auto border-l p-4 pb-8 max-md:hidden lg:w-xs xl:w-sm"
+      >
+        <p class="text-2xl font-bold">{{ TITLE }}</p>
+
+        <!-- Step 1: Select Image -->
+        <div class="flex flex-col gap-2">
+          <p
+            class="text-xl font-semibold"
+            :class="[imagePath && 'flex items-center-safe gap-2']"
+          >
+            Step 1
+            <Check v-if="imagePath" :size="18" class="text-primary" />
+          </p>
+          <Button class="self-start" variant="secondary" @click="selectImage()">
+            <template v-if="imagePath">Change</template>
+            <template v-else>Select</template> Image
+          </Button>
+        </div>
+
+        <!-- Step 2: Select Format -->
+        <div class="flex flex-col gap-2">
+          <p
+            class="text-xl font-semibold"
+            :class="[toFormat && 'flex items-center-safe gap-2']"
+          >
+            Step 2
+            <Check v-if="toFormat" :size="18" class="text-primary" />
+          </p>
+          <p class="text-sm">Select format to convert to</p>
+          <Select v-model="toFormat">
+            <SelectTrigger>
+              <SelectValue placeholder="Select Format" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Formats</SelectLabel>
+                <SelectItem value="jpg">JPG</SelectItem>
+                <SelectItem value="png">PNG</SelectItem>
+                <SelectItem value="webp">WEBP</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+
+        <!-- Step 3: Set Output Directory -->
+        <div class="flex flex-col gap-2">
+          <p
+            class="text-xl font-semibold"
+            :class="[outputDir && 'flex items-center-safe gap-2']"
+          >
+            Step 3
+            <Check v-if="outputDir" :size="18" class="text-primary" />
+          </p>
+          <p v-if="!outputDir" class="text-sm">Defaults to image's path</p>
+          <div class="flex gap-2">
+            <Button variant="secondary" @click="setOutputDir">
+              <template v-if="outputDir">Change Folder</template>
+              <template v-else>Set Output Folder</template>
+            </Button>
+            <Button
+              v-if="outputDir"
+              variant="secondary"
+              size="icon"
+              @click="outputDir = ''"
+            >
+              <X />
+            </Button>
+          </div>
+          <p
+            v-if="outputDir"
+            class="font-mono text-sm wrap-anywhere opacity-40"
+          >
+            {{ outputDir }}
+          </p>
+        </div>
+
+        <!-- Step 4: Fourmat (Format and Save) -->
+        <div class="flex flex-col gap-2">
+          <p class="text-xl font-semibold">Step 4</p>
+          <Button class="self-start" @click="save" :disabled="isSaving">
+            <Cpu />
+            Fourmat
+          </Button>
+        </div>
+      </div>
+    </main>
+  </TooltipProvider>
   <Toaster richColors :theme="$colorMode.value == 'dark' ? 'dark' : 'light'" />
 </template>
