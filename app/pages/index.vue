364bbb4e1919a4201af0b1fdfd353732420c21a9
zorvi <aervxa@gmail.com>
2026-02-11 05:53:05 +0300

    Add title and "credit"

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -11,17 +11,33 @@ const ROUTES = [
 </script>
 
 <template>
-  <!-- Grid of all tools -->
-  <main
-    class="grid size-full auto-rows-min grid-cols-[repeat(auto-fill,minmax(256px,1fr))] gap-4 p-8"
-  >
-    <LazyNuxtLink
-      v-for="route in ROUTES"
-      :href="route.href"
-      class="flex aspect-square size-full flex-col gap-4 rounded-2xl border-2 p-4"
+  <main class="flex size-full flex-col">
+    <p class="px-8 pt-6 text-center text-3xl font-bold tracking-wide">
+      Fourmat Tools
+    </p>
+    <!-- Credits lol -->
+    <p
+      class="mt-1 px-4 pb-3 text-center font-mono text-xs tracking-wider opacity-40"
+      :style="{
+        wordSpacing: '2px',
+      }"
     >
-      <Component :is="route.icon" class="size-full p-4 opacity-40" />
-      <p class="text-center text-2xl font-semibold">{{ route.title }}</p>
-    </LazyNuxtLink>
+      Built by
+      <!-- TODO Open link in native browser -->
+      <a href="https://www.github.com/aervxa" target="_blank">zorvi</a>
+    </p>
+    <!-- Grid of all tools -->
+    <div
+      class="grid flex-1 auto-rows-min grid-cols-[repeat(auto-fill,minmax(256px,1fr))] gap-4 p-8"
+    >
+      <LazyNuxtLink
+        v-for="route in ROUTES"
+        :href="route.href"
+        class="flex aspect-square size-full flex-col gap-4 rounded-2xl border-2 p-4"
+      >
+        <Component :is="route.icon" class="size-full p-4 opacity-40" />
+        <p class="text-center text-2xl font-semibold">{{ route.title }}</p>
+      </LazyNuxtLink>
+    </div>
   </main>
 </template>
