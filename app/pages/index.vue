9899f0f891825d95f78a92e7386bdda8c95f4920
zorvi <aervxa@gmail.com>
2026-02-11 04:27:08 +0300

    Convert dummy link to actual grid of tools

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,27 @@
+<script setup lang="ts">
+import { Image } from "lucide-vue-next";
+
+const ROUTES = [
+  {
+    title: "Image Converter",
+    href: "/image-converter",
+    icon: Image,
+  },
+];
+</script>
+
 <template>
-  <!-- FIXME: this is temporary until sidebar is built and actual content is able to be put here -->
-  <a href="/image-converter">go here</a>
+  <!-- Grid of all tools -->
+  <main
+    class="grid size-full auto-rows-min grid-cols-[repeat(auto-fill,minmax(256px,1fr))] gap-4 p-8"
+  >
+    <a
+      v-for="route in ROUTES"
+      :href="route.href"
+      class="flex aspect-square size-full flex-col gap-4 rounded-2xl border-2 p-4"
+    >
+      <Component :is="route.icon" class="size-full p-4 opacity-40" />
+      <p class="text-center text-2xl font-semibold">{{ route.title }}</p>
+    </a>
+  </main>
 </template>
