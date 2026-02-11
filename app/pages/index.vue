e46f8168f8c40819c50484ead1866f0968608414
zorvi <aervxa@gmail.com>
2026-02-11 05:34:15 +0300

    use NuxtLink instead of a tag to use client routing

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -15,13 +15,13 @@ const ROUTES = [
   <main
     class="grid size-full auto-rows-min grid-cols-[repeat(auto-fill,minmax(256px,1fr))] gap-4 p-8"
   >
-    <a
+    <LazyNuxtLink
       v-for="route in ROUTES"
       :href="route.href"
       class="flex aspect-square size-full flex-col gap-4 rounded-2xl border-2 p-4"
     >
       <Component :is="route.icon" class="size-full p-4 opacity-40" />
       <p class="text-center text-2xl font-semibold">{{ route.title }}</p>
-    </a>
+    </LazyNuxtLink>
   </main>
 </template>
