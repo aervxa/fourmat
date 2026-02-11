b3a934202d7df13fb15c337358069de9351351c7
zorvi <aervxa@gmail.com>
2026-02-11 06:00:20 +0300

    animate index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { Image } from "lucide-vue-next";
 import { openUrl } from "@tauri-apps/plugin-opener";
+import { motion } from "motion-v";
 
 const ROUTES = [
   {
@@ -13,11 +14,25 @@ const ROUTES = [
 
 <template>
   <main class="flex size-full flex-col">
-    <p class="px-8 pt-6 text-center text-3xl font-bold tracking-wide">
+    <motion.p
+      :initial="{
+        y: '-100%',
+      }"
+      :animate="{
+        y: 0,
+      }"
+      class="px-8 pt-6 text-center text-3xl font-bold tracking-wide"
+    >
       Fourmat Tools
-    </p>
+    </motion.p>
     <!-- Credits lol -->
-    <p
+    <motion.p
+      :initial="{
+        opacity: 0,
+      }"
+      :animate="{
+        opacity: 1,
+      }"
       class="mt-1 px-4 pb-3 text-center font-mono text-xs tracking-wider opacity-40"
       :style="{
         wordSpacing: '2px',
@@ -31,18 +46,24 @@ const ROUTES = [
       >
         zorvi
       </Button>
-    </p>
+    </motion.p>
     <!-- Grid of all tools -->
     <div
       class="grid flex-1 auto-rows-min grid-cols-[repeat(auto-fill,minmax(256px,1fr))] gap-4 p-8"
     >
-      <LazyNuxtLink
-        v-for="route in ROUTES"
-        :href="route.href"
-        class="flex aspect-square size-full flex-col gap-4 rounded-2xl border-2 p-4"
-      >
-        <Component :is="route.icon" class="size-full p-4 opacity-40" />
-        <p class="text-center text-2xl font-semibold">{{ route.title }}</p>
+      <LazyNuxtLink v-for="route in ROUTES" :href="route.href" class="contents">
+        <motion.div
+          :initial="{
+            scale: 0.9,
+          }"
+          :animate="{
+            scale: 1,
+          }"
+          class="flex aspect-square size-full flex-col gap-4 rounded-2xl border-2 p-4"
+        >
+          <Component :is="route.icon" class="size-full p-4 opacity-40" />
+          <p class="text-center text-2xl font-semibold">{{ route.title }}</p>
+        </motion.div>
       </LazyNuxtLink>
     </div>
   </main>
