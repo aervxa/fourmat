6daa7370bb58ef5f94190ce84f561dfffe763447
zorvi <aervxa@gmail.com>
2026-02-13 18:52:25 +0300

    Add buttons to header

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,14 +1,28 @@
 <script setup lang="ts">
-import { useSidebar } from "@/components/ui/sidebar";
-import { PanelLeft, X } from "lucide-vue-next";
-
-const { toggleSidebar } = useSidebar();
+import { X, ChevronLeft, ChevronRight } from "lucide-vue-next";
 </script>
 
 <template>
   <div class="flex h-(--header-height) shrink-0 justify-between">
-    <div class="flex aspect-square items-center justify-center">
-      <SidebarTrigger />
+    <div class="flex items-center">
+      <div class="flex aspect-square h-full items-center justify-center">
+        <SidebarTrigger />
+      </div>
+
+      <Separator orientation="vertical" class="max-h-1/2" />
+
+      <div class="ml-2 flex gap-1">
+        <!-- Forward and Back -->
+        <!-- TODO: Get it to actually work -->
+        <Button variant="ghost" size="icon" class="size-7">
+          <ChevronLeft class="size-6" />
+          <span class="sr-only">Go back</span>
+        </Button>
+        <Button variant="ghost" size="icon" class="size-7">
+          <ChevronRight class="size-6" />
+          <span class="sr-only">Go forward</span>
+        </Button>
+      </div>
     </div>
     <div class="flex aspect-square items-center justify-center">
       <!-- TODO: Close app -->
