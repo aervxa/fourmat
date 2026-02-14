03de2395a457e54e8592c36af8c3dc472994a9bb
zorvi <aervxa@gmail.com>
2026-02-14 16:06:32 +0300

    Show window after nuxt mounts to prevent white (empty) loading screen

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import { invoke } from "@tauri-apps/api/core";
+
+onMounted(async () => {
+  await invoke("show_window");
+});
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,15 @@
+use tauri::Manager;
+
 mod image_converter;
+
+// To be called once the app has initialized
+#[tauri::command]
+fn show_window(app: tauri::AppHandle) {
+    app.get_webview_window("main")
+        .expect("Failed to get window 'main'")
+        .show()
+        .expect("Failed to show window 'main'");
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -15,7 +26,10 @@ pub fn run() {
             }
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![image_converter::convert_images])
+        .invoke_handler(tauri::generate_handler![
+            show_window,
+            image_converter::convert_images
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
